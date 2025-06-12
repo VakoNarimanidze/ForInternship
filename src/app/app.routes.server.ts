0d3be07436d70b893ab11d53736021f04f4ed2fb
VakoNarimanidze <119787310+VakoNarimanidze@users.noreply.github.com>
2025-06-12 14:37:54 +0400

    .

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,7 +2,24 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path:'',
+    renderMode:RenderMode.Prerender
+  },
+  {
+    path:'users',
+    renderMode:RenderMode.Prerender
+  },
+  {
+    path:'posts',
+    renderMode:RenderMode.Prerender
+  },
+  {
+    path:'posts/:id',
+    renderMode:RenderMode.Prerender
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
+  
 ];
