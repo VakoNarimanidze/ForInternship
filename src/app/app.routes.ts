779eba3f2d1c793d6b960b2b6e0c6310c44e92b2
VakoNarimanidze <119787310+VakoNarimanidze@users.noreply.github.com>
2025-06-12 15:33:24 +0400

    .

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,15 @@ import { Routes } from '@angular/router';
 import { UsersComponent } from './users/users.component';
 import { PostsComponent } from './posts/posts.component';
 import { HomepageComponent } from './homepage/homepage.component';
+import { RenderMode } from '@angular/ssr';
 
 export const routes: Routes = [
     {path:'',component:HomepageComponent},
     {path: 'users',component:UsersComponent},
     { path: 'posts', component: PostsComponent }, 
+    {path: 'posts/:id',component: PostsComponent,
+      data: { RenderMode:'client'}
+    },
     {
   path: 'todos/:id',
   loadComponent: () => import('./todos/todos.component').then(m => m.TodosComponent)
